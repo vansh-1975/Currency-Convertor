6310fec5903d4a09267d8f274be4299928395ccb
Vansh <ribisht2001@gmail.com>
2025-12-15 08:37:15 +0530

    Initial project upload

---
+++ currConvertor.js
@@ -0,0 +1,56 @@
+
+let dropdowns=document.querySelectorAll(".dropdown select");
+let btn=document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg=document.querySelector(".msg");
+
+window.addEventListener("load",()=>{
+    exchangeUpdateRate();
+})
+
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name==="from"&&currCode==="USD")
+            newOption.selected="selected";
+        else if(select.name==="to"&&currCode==="INR")
+            newOption.selected="selected";
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag=(element)=>{
+    let currCode=element.value;
+    let countryCode=countryList[currCode];
+    let newSrc=`https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let image=element.parentElement.querySelector("img");
+    image.src=newSrc;
+}
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    exchangeUpdateRate();
+});
+
+const exchangeUpdateRate=async()=>{
+    let amount=document.querySelector(".amount input");
+    let amtVal=amount.value;
+    if(amtVal===""||amtVal<1){
+        amtVal=1;
+        amount.value=1;
+    }
+    const URL=`https://api.frankfurter.dev/v1/latest?from=${fromCurr.value}&to=${toCurr.value}`;
+    let response=await fetch(URL);
+    let data=await response.json();
+    let rate=data.rates[toCurr.value];
+    let finalAmt=rate*amtVal;
+    msg.innerText=`${amtVal}  ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+     
+}
